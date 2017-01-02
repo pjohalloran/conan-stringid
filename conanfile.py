fdc666a5f9aeb8fbd770d384fb663ffe6f88b095
pjohalloran <pjohalloran@gmail.com>
2017-01-02 22:17:51 +0000

    Adding lib flags.

--- conanfile.py
+++ conanfile.py
@@ -25,3 +25,9 @@ class StringIdConan(ConanFile):
     self.copy("*.hpp.in", dst="include", keep_path=False)
     self.copy("*.lib", dst="lib", keep_path=False)
     self.copy("*.a", dst="lib", keep_path=False)
+
+  def package_info(self):
+    self.cpp_info.sharedlinkflags = ["-std=c++11"]
+    self.cpp_info.exelinkflags = ["-std=c++11"]
+    self.cpp_info.libs = ["string_id", "stdc++"]
+    self.cpp_info.cppflags = ["-std=c++11", "-stdlib=libc++"]
