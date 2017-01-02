e0078f53b4d2d3f9c09b6b95be8dec5b91b26bd2
pjohalloran <pjohalloran@gmail.com>
2017-01-02 21:55:04 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# conan-stringid
+ Packaging the string_id (https://github.com/foonathan/string_id) library for the Conan package manager (http://conan.io) 
+
+## Notes
+- Package tested and known to work on OSX so far. Hopefully it will also work on other platforms, but pull requests are very welcome if not.
---
+++ conanfile.py
@@ -0,0 +1,27 @@
+from conans import ConanFile, CMake
+import os
+
+class StringIdConan(ConanFile):
+  name = "string_id"
+  version = "2.0-2"
+  description = "A small C++ library to handle hashed strings serving as identifiers."
+  license="Modified BSD License (3-Clause BSD license)"
+  settings = "os", "compiler", "build_type", "arch"
+  url = "https://github.com/pjohalloran/conan-stringid"
+
+  def source(self):
+    self.run("git clone https://github.com/foonathan/string_id")
+    os.chdir("string_id")
+    self.run("git checkout v2.0-2")
+
+  def build(self):
+    os.makedirs("string_id/build")
+    os.chdir("string_id/build")
+    self.run("cmake ..")
+    self.run("cmake --build .")
+
+  def package(self):
+    self.copy("*.hpp", dst="include", keep_path=True)
+    self.copy("*.hpp.in", dst="include", keep_path=True)
+    self.copy("*.lib", dst="lib", keep_path=False)
+    self.copy("*.a", dst="lib", keep_path=False)
